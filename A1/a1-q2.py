677597a6add95816001b43fe7c8d265befecfef2
austinvuong <austin.vuong@hotmail.com>
2023-05-26 14:13:12 -0400

    Minor corrections and refactoring
    Corrections were made to computing the covariance ellipse and variant of how the semi-axes can be computed is included.

--- A1/a1-q2.py
+++ A1/a1-q2.py
@@ -10,7 +10,7 @@ num_samples = 1000
 mean = np.array([1, 0.5])
 mean[1] = np.radians(mean[1])
 
-cov = np.array([[0.01, 0], [0, 0.005]])
+cov = np.array([[0.01, 0], [0, 1.00]])
 cov[1, 1] = np.radians(cov[1, 1])
 
 # Constructed this way to reduce errors and improve readability
@@ -23,10 +23,10 @@ def cov_y_builder(mean, cov):
    c = np.sin(theta)
    d = rho * np.cos(theta)
 
-   e = cov[0, 0] ** 2
-   f = cov[0, 1] ** 2
-   g = cov[1, 0] ** 2
-   h = cov[1, 1] ** 2
+   e = cov[0, 0]
+   f = cov[0, 1]
+   g = cov[1, 0]
+   h = cov[1, 1]
 
    cov_y = np.zeros((2, 2))
    cov_y[0, 0] = (a*e+b*g)*a+(a*f+b*h)*b
@@ -38,25 +38,34 @@ def cov_y_builder(mean, cov):
 # Generate samples
 polar_samples = np.random.multivariate_normal(mean, cov, num_samples)
 
-# Create the polar to cartesian transform
-transform = lambda rho, theta: (rho * np.cos(theta), rho * np.sin(theta))
-transform_v = np.vectorize(transform)
+# Create the polar to cartesian transform and perform it
+transform_v = np.vectorize(lambda rho, theta: (rho * np.cos(theta), rho * np.sin(theta)))
 
-x_coords, y_coords = transform_v(polar_samples[:, 0], polar_samples[:, 1])
+cartesian_mean = transform_v(mean[0], mean[1])
+cartesian_coords = transform_v(polar_samples[:, 0], polar_samples[:, 1])
 
 # Compute the eigenvalues and eignevectors of the transformed covariance
 cov_y = cov_y_builder(mean, cov)
 eigenvalues, eigenvectors = np.linalg.eig(cov_y)
 
 # Create the uncertaintly ellipse
-s = 5.991 # 95%
-width = 2 * np.sqrt(cov[0, 0] * s)
-height = 2 * np.sqrt(cov[1, 1] * s)
-angle = np.degrees(np.arctan2(eigenvectors[0,1], eigenvectors[0,0]))
-ellipse = Ellipse(xy=transform(mean[0], mean[1]), width=width, height=height, angle=angle, alpha=0.3, color='r', fill=False)
+
+# Simple confidence variant
+s = np.sqrt(-2 * np.log(1 - 0.95)) # simple 
+major = 2 * np.sqrt(np.max(eigenvalues)) * s
+minor = 2 * np.sqrt(np.min(eigenvalues)) * s
+
+# chi-squared variant (used in submission)
+# s = 5.991 # 95% confidence
+# major = 2 * np.sqrt(np.max(eigenvalues) * s) 
+# minor = 2 * np.sqrt(np.min(eigenvalues) * s)
+
+max_eigenvalue_index = np.argmax(eigenvalues)
+angle = -np.degrees(np.arctan2(eigenvectors[max_eigenvalue_index,1], eigenvectors[max_eigenvalue_index,0]))
+ellipse = Ellipse(xy=cartesian_mean, width=major, height=minor, angle=angle, alpha=0.3, color='r', fill=False)
 
 # Plotting
-sns.scatterplot(x=x_coords, y=y_coords, s=3)
+sns.scatterplot(x=cartesian_coords[0], y=cartesian_coords[1], s=3)
 plt.gca().add_patch(ellipse)
 
 plt.xlabel('X')
@@ -66,8 +75,8 @@ plt.title('')
 plt.axis('equal')
 # Center on origin
 # Comment out to center on the mean
-# plot_scale = 1.5
-# plt.xlim(-plot_scale, plot_scale)
-# plt.ylim(-plot_scale, plot_scale)
+plot_scale = 1.5
+plt.xlim(-plot_scale, plot_scale)
+plt.ylim(-plot_scale, plot_scale)
 
 plt.show()
