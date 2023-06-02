5d7e42c806578f228e9284e768857cdd3898cae0
austinvuong <austin.vuong@hotmail.com>
2023-06-02 13:32:11 -0400

    Remove linear measurement and do clean up
    Having both linear and non-linear models made for a cluttered visual

--- A2/a2-q2b.py
+++ A2/a2-q2b.py
@@ -2,12 +2,14 @@ import pygame
 import numpy as np
 from numpy.linalg import inv
 
-SCALE_FACTOR = 480 * .9
-PREDICTIONS_PER_SECOND = 8
+# Config
+SCALE_FACTOR = 480 * .8
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 480
 
-np.random.seed(42)
+PREDICTIONS_PER_SECOND = 8
+
+np.random.seed(43)
 
 # pygame setup
 pygame.init()
@@ -16,7 +18,6 @@ screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) # the window
 world_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA) # the main drawing surface
 trajectory_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA) # for drawing trajectory of the robot
 uncertainty_surface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA) # for drawing uncertainty ellipses
-last_position = np.zeros((2, 1)) # track the last position for drawing the trajectory
 
 clock = pygame.time.Clock()
 running = True
@@ -28,16 +29,17 @@ offset = pygame.Vector2(WINDOW_WIDTH / 2 - point_M[0, 0] * SCALE_FACTOR, WINDOW_
 
 # robot
 state = np.zeros((3, 1))
-state[0] = point_M[0]
-state[1] = point_M[1] - .3
+# manually place the robot a bit north of point M
+state[0] = point_M[0] 
+state[1] = point_M[1] - .4
+last_state = state.copy() # for trajectory visualization
 r = .1 # wheel radius (and also the speed in this case)
 L = .3 # wheel base
 
+# Motion model
 A = np.identity(3) # transition matrix
-
-# motion matrix
-B = np.identity(3)
-def update_B(theta, r, L):
+B = np.identity(3) # motion matrix
+def update_B(theta, r, L): # updates the motion matrix with 
     B[0, 0] = r * np.cos(theta)
     B[1, 1] = r * np.sin(theta)
     B[2, 2] = r/L
@@ -53,47 +55,39 @@ def update_u(u_r, u_l):
 
 # State Prediction (odometry)
 state_pred = state.copy()
-P_pred = np.zeros((3, 3))
-P = np.zeros((3, 3)) # prediction (position) covariance
+last_state_pred = state_pred.copy() # for trajectory visualization
+P_pred = np.zeros((3, 3)) # prediction covariance
 sd_w_omgea = .1
 sd_w_psi = .01
-Q = np.array([
-    [sd_w_omgea**2,             0,           0],
-    [            0, sd_w_omgea**2,           0],
-    [            0,             0, sd_w_psi**2]
+R = np.array([
+    [sd_w_omgea,             0,           0],
+    [            0, sd_w_omgea,           0],
+    [            0,             0, sd_w_psi]
 ]) # process covariance
 
-# State Update (measurement) (from A2a)
+# State Update (landmark) (for Q2b)
 state_corrected = np.zeros((2, 1))
+last_state_corrected = state_corrected.copy()
 P_corrected = np.zeros((3, 3))
-C = np.zeros((2, 3)) # observation matrix
-C[0, 0] = 1
-C[1, 1] = 2
-sd_r_x = .05
-sd_r_y = .075
-R = np.array([[sd_r_x**2, 0], [0, sd_r_y**2]]) # measurement covariance
-
-# State Update (landmark) (from A2b)
-state_landmark_corrected = np.zeros((2, 1))
-P_landmark_corrected = np.zeros((3, 3))
-D = np.zeros((2, 3)) # landmark matrix
-def update_D(x, y):
-    # d1 = rho = (range) = sqrt(X^2 + Y^2)
-    # d2 = phi = (bearing) = arctan(Y/X)
+C = np.zeros((2, 3)) # landmark matrix
+def update_C(x, y):
+    # The system is:
+    # rho = (range) = sqrt(X^2 + Y^2)
+    # phi = (bearing) = arctan(Y/X)
     X = point_M[0] - x
     Y = point_M[1] - y
     X2Y2 = X ** 2 + Y ** 2
 
     # The Jacobian
-    D[0, 0] = Y / np.sqrt(X2Y2) # del d1 / del x
-    D[0, 1] = X / np.sqrt(X2Y2) # del d1 / del y
-    D[1, 1] = Y / X2Y2 # del d2 / del x
-    D[1, 1] = X / X2Y2 # del d2 / del y
-    return D
+    C[0, 0] = Y / np.sqrt(X2Y2) # del rho / del x
+    C[0, 1] = X / np.sqrt(X2Y2) # del rho / del y
+    C[1, 1] = Y / X2Y2 # del phi / del x
+    C[1, 1] = X / X2Y2 # del phi / del y
+    return C
 
 sd_range = .1
 sd_bearing = .01
-S = np.array([[sd_range**2, 0], [0, sd_bearing**2]]) # landmark covariance
+Q = np.array([[sd_range, 0], [0, sd_bearing]]) # landmark covariance
 
 # Timers
 prediction_timer_delay = round(1000/PREDICTIONS_PER_SECOND)
@@ -103,10 +97,6 @@ pygame.time.set_timer(PREDICTION_TIMER_EVENT, prediction_timer_delay)
 measurement_timer_delay = 1000
 MEASUREMENT_TIMER_EVENT = pygame.USEREVENT + 1
 pygame.time.set_timer(MEASUREMENT_TIMER_EVENT, measurement_timer_delay)
-
-landmark_timer_delay = 700 # chossen avoid overlapping the measurement timer
-LANDMARK_TIMER_EVENT = pygame.USEREVENT + 2
-pygame.time.set_timer(LANDMARK_TIMER_EVENT, landmark_timer_delay)
 
 # Taken from https://stackoverflow.com/questions/65767785/how-to-draw-a-rotated-ellipse-using-pygame
 def draw_ellipse_angle(surface, color, rect, angle, width=0):
@@ -126,7 +116,7 @@ def draw_covariance_ellipse(state, P, confidence=.95, color='purple'):
 
     s = np.sqrt(-2 * np.log(1 - confidence)) # simple 
     major = 2 * np.sqrt(np.max(eigenvalues) * SCALE_FACTOR) * s
-    minor = 2 * np.sqrt(np.min(eigenvalues[eigenvalues > 0.00001]) * SCALE_FACTOR) * s
+    minor = 2 * np.sqrt(np.min(eigenvalues) * SCALE_FACTOR) * s
 
     max_eigenvalue_index = np.argmax(eigenvalues)
     angle = -np.degrees(np.arctan2(eigenvectors[max_eigenvalue_index, 1], eigenvectors[max_eigenvalue_index, 0]))
@@ -135,6 +125,10 @@ def draw_covariance_ellipse(state, P, confidence=.95, color='purple'):
             state[1, 0] * SCALE_FACTOR - minor/2 + offset.y, 
             major, minor)
     draw_ellipse_angle(uncertainty_surface, color, rect, angle, 2)
+
+# Set the control inputs
+update_u(.7, .3)
+u *= 5 #TEMP to speed up motion
 
 while running:
     # poll for events
@@ -145,49 +139,30 @@ while running:
         elif event.type == PREDICTION_TIMER_EVENT:
             # print('prediction')
             vel_pred = B @ u / PREDICTIONS_PER_SECOND
-            P_pred = A @ P_pred @ A.T + Q # (3.120a) modified for multiple predictions
+            P_pred = A @ P_pred @ A.T + R # (3.120a) modified for multiple predictions
             state_pred = A @ state_pred + vel_pred # (3.120b) modified for multiple predictions
 
         elif event.type == MEASUREMENT_TIMER_EVENT:
             # print('measurement')
-            y = C @ state
+            update_C(*state[0:2])
 
-            K = P_pred @ C.T @ inv(C @ P_pred @ C.T + R)
+            z = C @ state
+            innovation = (z - C @ state_pred)
 
-            innovation = (y - C @ state_pred)
-            # print(f'K: {K}')
-            # print(f'innovation: {innovation}')
+            K = P_pred @ C.T @ inv(C @ P_pred @ C.T + Q)
             
             P_corrected = (np.identity(3) - K @ C) @ P_pred
             state_corrected = state_pred + K @ innovation
             
             P_pred = P_corrected
             state_pred = state_corrected
-        elif event.type == LANDMARK_TIMER_EVENT:
-            # print('measurement')
-            update_D(*state[0:2])
-            y = D @ state
 
-            K = P_pred @ D.T @ inv(D @ P_pred @ D.T + S)
-
-            innovation = (y - D @ state_pred)
-            # print(f'K: {K}')
-            # print(f'innovation: {innovation}')
-            
-            P_landmark_corrected = (np.identity(3) - K @ D) @ P_pred
-            state_landmark_corrected = state_pred + K @ innovation
-            
-            P_pred = P_landmark_corrected
-            state_pred = state_landmark_corrected
 
     # Simulate ground truth
-    d_state = np.random.multivariate_normal((B @ u).flatten(), Q).reshape(-1, 1) # with noisy motion
+    d_state = np.random.multivariate_normal((B @ u).flatten(), R).reshape(-1, 1) # with noisy motion
     # d_state = B @ u # with noiseless motion
-    # d_state[2] = np.radians(d_state[2])
     state += d_state * dt
 
-    update_u(.7, .3)
-    u *= 5 #TEMP to speed up tests
     update_B(state[2], r, L)
 
     # Draw
@@ -195,24 +170,32 @@ while running:
     world_surface.fill('white')
     uncertainty_surface.fill((0, 0, 0, 0))
 
-    # Trajectory line
-    pygame.draw.line(trajectory_surface, 'lightblue', 
-        pygame.Vector2(*last_position) * SCALE_FACTOR + offset,
+    # Trajectory lines
+    pygame.draw.line(trajectory_surface, 'skyblue', 
+        pygame.Vector2(*last_state[0:2]) * SCALE_FACTOR + offset,
         pygame.Vector2(*state[0:2]) * SCALE_FACTOR + offset)
-    last_position = state[0:2].copy()
+    last_state = state.copy()
+
+    pygame.draw.line(trajectory_surface, 'lightgreen', 
+        pygame.Vector2(*last_state_pred[0:2]) * SCALE_FACTOR + offset,
+        pygame.Vector2(*state_pred[0:2]) * SCALE_FACTOR + offset)
+    last_state_pred = state_pred.copy()
+
+    pygame.draw.line(trajectory_surface, 'darksalmon', 
+        pygame.Vector2(*last_state_corrected[0:2]) * SCALE_FACTOR + offset,
+        pygame.Vector2(*state_corrected[0:2]) * SCALE_FACTOR + offset)
+    last_state_corrected = state_corrected.copy()
 
     # Ground truth
     pygame.draw.circle(world_surface, 'blue', pygame.Vector2(*state[0:2]) * SCALE_FACTOR + offset, 3, width=0)
 
-    # point M
+    # Point M
     pygame.draw.circle(world_surface, 'black', pygame.Vector2(*point_M) * SCALE_FACTOR + offset, 3, width=0)
 
     # Uncertainty
     draw_covariance_ellipse(state_pred, P_pred, confidence=.95, color='green')
-    draw_covariance_ellipse(state_pred, P_pred, confidence=.5, color='lightgreen')
-    draw_covariance_ellipse(state_corrected, P_corrected, confidence=.95, color='red')
-    draw_covariance_ellipse(state_landmark_corrected, P_landmark_corrected, confidence=.95, color='orange')
-    
+    draw_covariance_ellipse(state_corrected, P_corrected, confidence=.95, color='red') # indexed to only include what the measurement can read
+
     # blit and flip
     screen.blit(world_surface, (0, 0))
     screen.blit(trajectory_surface, (0, 0))
