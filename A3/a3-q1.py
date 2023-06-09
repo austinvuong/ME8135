2ac168d0fea4d5e0422dea684aa2b9475ed3dbe9
austinvuong <austin.vuong@hotmail.com>
2023-06-09 11:00:31 -0400

    Clean up

--- A3/a3-q1.py
+++ A3/a3-q1.py
@@ -1,16 +1,15 @@
 import pygame
 import numpy as np
-from numpy.linalg import inv
 from scipy.stats import multivariate_normal
 from dataclasses import dataclass
 
 # Config
-SCALE_FACTOR = 480*.5
+SCALE_FACTOR = 480*2.
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 480
 
 PREDICTIONS_PER_SECOND = 8
-NUMBER_OF_PARTICLES = 100
+NUMBER_OF_PARTICLES = 30
 
 np.random.seed(42) # fix for reproducution
 
@@ -26,7 +25,7 @@ _clock = pygame.time.Clock()
 _running = True
 _dt = 0
 
-_world_offset = pygame.Vector2(.5, .5) * SCALE_FACTOR # offset to position the world on the screen
+_world_offset = pygame.Vector2(0, 0) * SCALE_FACTOR # offset to position the world on the screen
 
 @dataclass
 class StateData:
@@ -47,27 +46,25 @@ class StateData:
 ground_truth = StateData(np.zeros((2, 1)), np.zeros((2, 2)))
 r = .1 # wheel radius (and also the speed in this case)
 
-# particles
-particles = np.zeros((NUMBER_OF_PARTICLES, 2, 1)) # particles[n] gets you the n'th particle as a column vector
-
 # Motion model
 transition_matrix = np.identity(2) # transition matrix
 motion_matrix = r/2 * np.identity(2) # motion matrix
 u_r = u_l = 1 # chosen control inputs
 control_vector = np.array([[u_r + u_l], [u_r + u_l]]) # control vector
 
-# Prediction (i.e. odometry)
+# Prediction (odometry)
 predicted = StateData(np.zeros((2, 1)), np.zeros((2, 2)))
+particles = np.zeros((NUMBER_OF_PARTICLES, 2, 1)) # particles[n] gets you the n'th particle as a column vector
 sd_w_x = .1
 sd_w_y = .15
 prediction_covariance = np.array([[sd_w_x, 0], [0, sd_w_y]]) # process covariance
 
-# Update (i.e. measurement)
-measurement = StateData(np.zeros((2, 1)), np.zeros((2, 2)))
+# Update (measurement)
 measurement_matrix = np.array([[1, 0], [0, 2]]) # observation matrix
 sd_r_x = .05
 sd_r_y = .075
 measurement_covariance = np.array([[sd_r_x, 0], [0, sd_r_y]]) # measurement covariance
+measurement = StateData(np.zeros((2, 1)), measurement_covariance)
 
 # Timers
 prediction_timer_delay = round(1000/PREDICTIONS_PER_SECOND)
@@ -100,7 +97,7 @@ def draw_covariance_ellipse(state_data: StateData, confidence=.95, color='magent
     minor = 2 * np.sqrt(np.min(eigenvalues) * SCALE_FACTOR) * s
     
     max_eigenvalue_index = np.argmax(eigenvalues)
-    angle = -np.degrees(np.arctan2(eigenvectors[max_eigenvalue_index, 1], eigenvectors[max_eigenvalue_index, 0]))
+    angle = np.degrees(np.arctan2(eigenvectors[max_eigenvalue_index, 1], eigenvectors[max_eigenvalue_index, 0]))
     
     rect = (state_data.state[0, 0] * SCALE_FACTOR - major/2 + _world_offset.x, 
             state_data.state[1, 0] * SCALE_FACTOR - minor/2 + _world_offset.y, 
@@ -127,50 +124,29 @@ while _running:
 
             particles += (vel_pred + noise) / PREDICTIONS_PER_SECOND
 
-            # compute for the uncertainty elipises
+            # compute for the uncertainty elipise
             predicted.update_state(np.mean(particles, axis=0))
             predicted.covariance = np.cov(np.reshape(particles, (len(particles), -1)), rowvar=False)
-            pass
+            
         elif event.type == MEASUREMENT_TIMER_EVENT:
             # # print('measurement')
-            # innovation =  - measurement_matrix @ predicted.state # C @ state - C @ state_pred
-            # K = (predicted.covariance @ measurement_matrix.T
-            #         @ inv(measurement_matrix @ predicted.covariance @ measurement_matrix.T + measureme5nt_covariance)
-            #     ) # P_pred @ C^T @ (C @ P_pred @ C^T + R)^-1
-
-            # # store for visualization
-            # measurement.covariance = (np.identity(2) - K @ measurement_matrix) @ predicted.covariance # (I - K @ C) @ P_pred
-            # measurement.update_state(predicted.state + K @ innovation) # state_pred + K @ innovation
-            
-            # # pass on the measurement
-            # predicted.covariance = measurement.covariance
-            # predicted.update_state(measurement.state)
-
-            #WIP
-
             z = measurement_matrix @ ground_truth.state
-            d = np.linalg.norm(z - particles, axis=1)
+            d = np.linalg.norm(z - particles, axis=1) # distance between measurement and particles
 
-            w = multivariate_normal.pdf(d, None, measurement_covariance) # unnormalized weights #TODO #WIP the problem is here, it is inverting the weights
+            w = multivariate_normal.pdf(d, None, measurement_covariance) # unnormalized weights
             w /= np.sum(w, axis=0) # normalize
             cdf = np.cumsum(w)
             
             indexes = np.searchsorted(cdf, np.random.rand(NUMBER_OF_PARTICLES))
             particles = [particles[i] for i in indexes]
 
+            # for the uncertainty elipise 
             measurement.update_state(z)
-            measurement.covariance = np.cov(np.reshape(particles, (len(particles), -1)), rowvar=False)
-            pass
-
-            #TODO figure out resampling
 
     # Simulate ground truth
     vel = motion_matrix @ control_vector # with noiseless motion
-    vel = np.random.multivariate_normal(vel.flatten(), prediction_covariance).reshape(-1, 1) # with noisy motion
-    ground_truth.update_state(ground_truth.state + vel * _dt)
-
-    #TEMP visualzing test
-    # particle += np.random.multivariate_normal(vel.flatten(), prediction_covariance).reshape(-1, 1) * _dt
+    vel2 = np.random.multivariate_normal(vel.flatten(), prediction_covariance).reshape(-1, 1) # with noisy motion
+    ground_truth.update_state(ground_truth.state + vel2 * _dt)
 
     # Draw
     # Background
@@ -182,11 +158,14 @@ while _running:
     draw_trajectory_line(predicted, 'lightgreen')
     draw_trajectory_line(measurement, 'darksalmon')
 
+    # Particles
+    for p in particles:
+        if p.min() < 0: # skip these particles to avoid the issue draws smearing across the surface
+            continue
+        pygame.draw.circle(_world_surface, 'magenta', pygame.Vector2(*p) * SCALE_FACTOR + _world_offset, 1, width=0)
+
     # Ground truth
     pygame.draw.circle(_world_surface, 'blue', pygame.Vector2(*ground_truth.state) * SCALE_FACTOR + _world_offset, 3, width=0)
-
-    # for p in particles: #TEMP
-    #     pygame.draw.circle(_world_surface, 'magenta', pygame.Vector2(*p) * SCALE_FACTOR + _world_offset, 1, width=0)
 
     # Uncertainty
     draw_covariance_ellipse(predicted, confidence=.95, color='green')
